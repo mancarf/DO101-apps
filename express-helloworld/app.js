58b810a1c622aac80af86feb25a736be7c8afb2b
MANUEL CARLOS FERNANDEZ GUTIERREZ <0007156@viewnext.com>
2020-07-01 19:33:08 +0200

    Cambio en caliente

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/mars', function (req, res) {
-  res.send('Hellos Mars!\n');
+  res.send('Hellos Mars! Cambio en caliente\n');
 });
 
 app.listen(8080, function () {
